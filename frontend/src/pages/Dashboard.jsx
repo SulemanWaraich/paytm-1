9ca9083d261f8b1776a05b34467d5a2ef3d83ec8
Muhammad Suleman <tech4you330@gmail.com>
2025-04-10 21:45:41 +0500

    Project Complete

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Heading from '../components/Heading';
 import NavBar from '../components/NavBar';
 import InputBox from '../components/InputBox';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { useNavigate } from 'react-router-dom';
+
 
 function Dashboard() {
-  const [users, setUsers] = useState(['Suleman', 'Ashfaque', 'Usama']);
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState('');
 
+  useEffect( () => {
+    axios.get("http://localhost:3000/api/v1/user/bulk?name=" + filter)
+    .then(res => {
+      setUsers(res.data.user)
+    });    
+  }, [filter]);
+
+  const navigate = useNavigate();
   return (
     <div>
       <NavBar />
     <div className='flex justify-center h-screen'>
       <div className='w-full px-4'>
       <Heading label={'Your Balance $5000'}/>
-      <InputBox name={'Users'} place={'Search users...'}/>
+      <InputBox name={'Users'} place={'Search users...'} onChange={(e) => {setFilter(e.target.value)}}/>
 
       <div>
         <ul className='pt-6 space-y-6 text-2xl'>
         {users.map((user) => {
-          return <li> <button className='border rounded-full bg-slate-200 border-slate-100 px-2 mx-2 font-semibold'>{user[0]}</button>{user}</li>})}
+          return <div className='flex justify-between'>
+            <li><button className='border rounded-full bg-slate-200 border-slate-100 px-2 mx-2 font-semibold'>{user.firstName[0]}</button>{user.firstName} {user.lastName}
+          </li>
+          
+          <Button onClick={(e) => navigate("/send?id=" + user._id + "&name=" + user.firstName)} variant="contained" endIcon={<SendIcon />}>
+          Send
+          </Button>
+          </div>
+        })}
         </ul>
       </div>
       </div>
--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../components/Heading'
 import SubHeading from '../components/SubHeading'
 import InputBox from '../components/InputBox'
 import Button from '../components/Button'
 import Friend from '../components/Friend'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 
 function Send() {
+  const [searchParams] = useSearchParams();
+  const [amount, setAmount] = useState('');
+  const id = searchParams.get("id");
+  const name = searchParams.get("name");
+
+  console.log(id, name);
+  
   return (
     <div className='h-screen flex justify-center items-center bg-slate-300'>
     <div className='bg-white rounded p-10 text-center w-96 space-y-2'>
       <div className='mb-4'>
       <Heading label={'Send Money'}/>
       </div>
-      <Friend capital={'MS'} name={'Muhammad Suleman'}/>
-      <InputBox name={'Amount (in Rs)'} place={'Enter Amount'}/>
-      <Button btnName={'Initiate Transfer'}/>
+      <Friend capital={name[0]} name={name}/>
+      <InputBox name={'Amount (in Rs)'} place={'Enter Amount'} onChange={(e) => {setAmount(e.target.value)}}/>
+      <Button onClick={async () => {
+        console.log(localStorage.getItem("token"));
+        console.log(amount);
+        
+        try {
+          const res = await axios.post("http://localhost:3000/api/v1/account/transfer", {
+          to: id,
+          amount
+        }, {headers: {
+          authorization: 'Bearer ' + localStorage.getItem("token")
+        }})
+        console.log(res.data);
+        } catch (error) { 
+          console.log('failed', error);
+          
+        }}} btnName={'Initiate Transfer'}/>
     </div>
     </div>
   )
